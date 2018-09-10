ceaf59078a60d7129168e11464e79c4eef7f4cba
xjm <925271237@qq.com>
2018-09-10 10:48:04 +0800

    c#控制摄像头

---
+++ Form1.Designer.cs
@@ -0,0 +1,247 @@
+﻿namespace OperateCamera
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.Photograph = new System.Windows.Forms.Button();
+            this.btnClose = new System.Windows.Forms.Button();
+            this.tscbxCameras = new System.Windows.Forms.ComboBox();
+            this.btnConnect = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.videoSourcePlayer = new AForge.Controls.VideoSourcePlayer();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.btnStartVideotape = new System.Windows.Forms.Button();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.toolStripStatusLabel1 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.lb_logs = new System.Windows.Forms.Label();
+            this.richText_logs = new System.Windows.Forms.RichTextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.tb_filePath = new System.Windows.Forms.TextBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.groupBox1.SuspendLayout();
+            this.statusStrip1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // Photograph
+            // 
+            this.Photograph.Location = new System.Drawing.Point(16, 61);
+            this.Photograph.Name = "Photograph";
+            this.Photograph.Size = new System.Drawing.Size(103, 39);
+            this.Photograph.TabIndex = 1;
+            this.Photograph.Text = "拍照";
+            this.Photograph.UseVisualStyleBackColor = true;
+            this.Photograph.Click += new System.EventHandler(this.Photograph_Click);
+            // 
+            // btnClose
+            // 
+            this.btnClose.Location = new System.Drawing.Point(161, 20);
+            this.btnClose.Name = "btnClose";
+            this.btnClose.Size = new System.Drawing.Size(118, 35);
+            this.btnClose.TabIndex = 3;
+            this.btnClose.Text = "关闭";
+            this.btnClose.UseVisualStyleBackColor = true;
+            this.btnClose.Click += new System.EventHandler(this.btnClose_Click);
+            // 
+            // tscbxCameras
+            // 
+            this.tscbxCameras.FormattingEnabled = true;
+            this.tscbxCameras.Location = new System.Drawing.Point(129, 12);
+            this.tscbxCameras.Name = "tscbxCameras";
+            this.tscbxCameras.Size = new System.Drawing.Size(171, 20);
+            this.tscbxCameras.TabIndex = 7;
+            // 
+            // btnConnect
+            // 
+            this.btnConnect.Location = new System.Drawing.Point(16, 20);
+            this.btnConnect.Name = "btnConnect";
+            this.btnConnect.Size = new System.Drawing.Size(103, 35);
+            this.btnConnect.TabIndex = 8;
+            this.btnConnect.Text = "连接";
+            this.btnConnect.UseVisualStyleBackColor = true;
+            this.btnConnect.Click += new System.EventHandler(this.btnConnect_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(89, 12);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "视频输入设备：";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // videoSourcePlayer
+            // 
+            this.videoSourcePlayer.Location = new System.Drawing.Point(309, 12);
+            this.videoSourcePlayer.Name = "videoSourcePlayer";
+            this.videoSourcePlayer.Size = new System.Drawing.Size(320, 240);
+            this.videoSourcePlayer.TabIndex = 10;
+            this.videoSourcePlayer.Text = "videoSourcePlayer";
+            this.videoSourcePlayer.VideoSource = null;
+            this.videoSourcePlayer.NewFrame += new AForge.Controls.VideoSourcePlayer.NewFrameHandler(this.videoSourcePlayer1_NewFrame);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.btnStartVideotape);
+            this.groupBox1.Controls.Add(this.btnConnect);
+            this.groupBox1.Controls.Add(this.btnClose);
+            this.groupBox1.Controls.Add(this.Photograph);
+            this.groupBox1.Location = new System.Drawing.Point(12, 38);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(288, 108);
+            this.groupBox1.TabIndex = 11;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "抓拍设置";
+            // 
+            // btnStartVideotape
+            // 
+            this.btnStartVideotape.Location = new System.Drawing.Point(161, 62);
+            this.btnStartVideotape.Name = "btnStartVideotape";
+            this.btnStartVideotape.Size = new System.Drawing.Size(118, 38);
+            this.btnStartVideotape.TabIndex = 9;
+            this.btnStartVideotape.Text = "开始录像";
+            this.btnStartVideotape.UseVisualStyleBackColor = true;
+            this.btnStartVideotape.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripStatusLabel1});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 347);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(638, 22);
+            this.statusStrip1.TabIndex = 12;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // toolStripStatusLabel1
+            // 
+            this.toolStripStatusLabel1.Name = "toolStripStatusLabel1";
+            this.toolStripStatusLabel1.Size = new System.Drawing.Size(56, 17);
+            this.toolStripStatusLabel1.Text = "准备就绪";
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 5000;
+            this.timer1.Tick += new System.EventHandler(this.Photograph_Click);
+            // 
+            // lb_logs
+            // 
+            this.lb_logs.AutoSize = true;
+            this.lb_logs.Location = new System.Drawing.Point(167, 59);
+            this.lb_logs.Name = "lb_logs";
+            this.lb_logs.Size = new System.Drawing.Size(0, 12);
+            this.lb_logs.TabIndex = 9;
+            // 
+            // richText_logs
+            // 
+            this.richText_logs.Location = new System.Drawing.Point(12, 258);
+            this.richText_logs.Name = "richText_logs";
+            this.richText_logs.Size = new System.Drawing.Size(617, 76);
+            this.richText_logs.TabIndex = 9;
+            this.richText_logs.Text = "";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(14, 62);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(263, 32);
+            this.button1.TabIndex = 9;
+            this.button1.Text = "设置照片存放路径";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // tb_filePath
+            // 
+            this.tb_filePath.Location = new System.Drawing.Point(14, 26);
+            this.tb_filePath.Name = "tb_filePath";
+            this.tb_filePath.Size = new System.Drawing.Size(263, 21);
+            this.tb_filePath.TabIndex = 10;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.button1);
+            this.groupBox2.Controls.Add(this.tb_filePath);
+            this.groupBox2.Location = new System.Drawing.Point(14, 153);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(286, 100);
+            this.groupBox2.TabIndex = 13;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "图片保存路径设置";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(638, 369);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.richText_logs);
+            this.Controls.Add(this.lb_logs);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.videoSourcePlayer);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.tscbxCameras);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Photograph;
+        private System.Windows.Forms.Button btnClose;
+        private System.Windows.Forms.ComboBox tscbxCameras;
+        private System.Windows.Forms.Button btnConnect;
+        private System.Windows.Forms.Label label1;
+        private AForge.Controls.VideoSourcePlayer videoSourcePlayer;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label lb_logs;
+        private System.Windows.Forms.RichTextBox richText_logs;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox tb_filePath;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Button btnStartVideotape;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,326 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing.Imaging;
+using System.Text;
+using System.Windows;
+using System.Windows.Forms;
+using System.IO;
+using System.Windows.Media.Imaging;
+using AForge;
+using AForge.Controls;
+using AForge.Video;
+using AForge.Video.DirectShow;
+using System.Timers;
+using System.Runtime.InteropServices;
+using System.Drawing;
+using AForge.Video.FFMPEG;
+
+
+namespace OperateCamera
+{
+    
+
+    public partial class Form1 : Form
+    {
+        private FilterInfoCollection videoDevices;
+        private VideoCaptureDevice videoSource;
+        private delegate void WriteLabelDelegate(object entry);
+
+        private bool stopREC = true;
+        private bool createNewFile = true;
+        int frameRate = 25; //默认帧率
+        private VideoFileWriter videoWriter = null;
+        private string videoFileFullPath = string.Empty; //视频文件全路径
+        private string imageFileFullPath = string.Empty; //图像文件全路径
+        private string videoPath = @"E:\video\"; //视频文件路径
+        private string videoFileName = string.Empty; //视频文件名
+        private string drawDate = string.Empty;
+        private VideoFileReader _reader;
+        private VideoFileWriter _writer;
+        static int intFlag = 0;
+
+        [DllImport("gdi32")]
+        static extern int DeleteObject(IntPtr o);
+
+        static System.Timers.Timer timerPic;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+            try
+            {
+                // 枚举所有视频输入设备
+                videoDevices = new FilterInfoCollection(FilterCategory.VideoInputDevice);
+
+                if (videoDevices.Count == 0)
+                    throw new ApplicationException();
+
+                foreach (FilterInfo device in videoDevices)
+                {
+                    tscbxCameras.Items.Add(device.Name);
+                }
+
+                tscbxCameras.SelectedIndex = 0;
+
+            }
+            catch (ApplicationException)
+            {
+                tscbxCameras.Items.Add("没有找到摄像透");
+                videoDevices = null;
+            }
+        }
+
+        private void btnConnect_Click(object sender, EventArgs e)
+        {
+            CameraConn();
+        }
+        //连接摄像头
+        private void CameraConn()
+        {
+            VideoCaptureDevice videoSource = new VideoCaptureDevice(videoDevices[tscbxCameras.SelectedIndex].MonikerString);
+            videoSource.DesiredFrameSize = new System.Drawing.Size(320, 240);
+            videoSource.DesiredFrameRate = 1;
+
+            videoSourcePlayer.VideoSource = videoSource;
+            videoSourcePlayer.Start();
+        }
+
+        //关闭摄像头
+        private void btnClose_Click(object sender, EventArgs e)
+        {
+            videoSourcePlayer.SignalToStop();
+            videoSourcePlayer.WaitForStop();
+        }
+
+        //主窗体关闭
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            btnClose_Click(null, null);
+        }
+
+        //拍照
+        private void Photograph_Click(object sender, EventArgs e)
+        {
+            GetPic();
+        }
+
+        /// <summary>
+        /// 拍照功能
+        /// </summary>
+        private void GetPic()
+        {
+            try
+            {
+                if (videoSourcePlayer.IsRunning)
+                {
+                    IntPtr ip = videoSourcePlayer.GetCurrentVideoFrame().GetHbitmap();
+                    this.richText_logs.Text = "[" + DateTime.Now + "]拍照功能运行中...";
+                    BitmapSource bitmapSource = System.Windows.Interop.Imaging.CreateBitmapSourceFromHBitmap(
+                                    ip,
+                                    IntPtr.Zero,
+                                     Int32Rect.Empty,
+                                    BitmapSizeOptions.FromEmptyOptions());
+                    DeleteObject(ip);
+                    PngBitmapEncoder pE = new PngBitmapEncoder();
+                    
+                    pE.Frames.Add(BitmapFrame.Create(bitmapSource));
+                    string picName = GetImagePath() + "\\" + "pic"+DateTime.Now.ToString("yyyyMMddhhmmss") + ".jpg";
+                    //string picName = GetImagePath() + "\\" + "cert.jpg";
+                    
+                    if (File.Exists(picName))
+                    {
+                        File.Delete(picName);
+                    }
+                    using (Stream stream = File.Create(picName))
+                    {
+                        pE.Save(stream);
+                        //SetLosResult("[" + DateTime.Now + "]拍照完成：" + picName);
+                        this.richText_logs.Text = "[" + DateTime.Now + "]拍照完成：" + picName;
+                    }
+                    //拍照完成后关摄像头并刷新同时关窗体
+                    //if (videoSourcePlayer != null && videoSourcePlayer.IsRunning)
+                    //{
+                    //    videoSourcePlayer.SignalToStop();
+                    //    videoSourcePlayer.WaitForStop();
+                    //}
+
+                    //this.Close();
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("摄像头异常：" + ex.Message);
+            }
+        }
+        private void videoSourcePlayer1_NewFrame(object sender, ref Bitmap image)
+        {
+            //Graphics gr = Graphics.FromImage(image);
+
+            //SolidBrush drawBrush = new SolidBrush(Color.Red);
+
+            //Font drawFont = new Font("Arial", 4, System.Drawing.FontStyle.Bold, GraphicsUnit.Millimeter);
+
+            //int xPos = 5;
+
+            //int yPos = 3;
+
+            //string date = System.DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss");//得到写到屏上的时间
+            //gr.DrawString(date, drawFont, drawBrush, xPos, yPos);
+
+            //if (stopREC)
+            //{
+            //    stopREC = true;
+            //    createNewFile = true;
+
+            //    if (videoWriter != null)
+            //    {
+            //        videoWriter.Close();
+            //    }
+            //}
+            //else
+            //{
+            //    if (createNewFile)
+            //    {
+                    
+            //        createNewFile = false;
+            //        if (videoWriter != null)
+            //        {
+            //            videoWriter.Close();
+            //            videoWriter.Dispose();
+            //        }
+            //        videoWriter = new VideoFileWriter();    
+            //        videoWriter.Open(DateTime.Now.ToString("yyMMddHH")+".avi", 320, 240, 20, VideoCodec.MPEG4);
+
+            //        // add the image as a new frame of video file
+            //        videoWriter.WriteVideoFrame(image);
+            //        videoWriter.Dispose();
+            //    }
+            //    else
+            //    {
+            //        videoWriter.WriteVideoFrame(image);
+            //    }
+            //}
+            //录像
+            Graphics g = Graphics.FromImage(image);
+            SolidBrush drawBrush = new SolidBrush(Color.Yellow);
+
+            Font drawFont = new Font("Arial", 6, System.Drawing.FontStyle.Bold, GraphicsUnit.Millimeter);
+            int xPos = image.Width - (image.Width - 15);
+            int yPos = 10;
+            //写到屏幕上的时间
+            drawDate = DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss");
+
+            g.DrawString(drawDate, drawFont, drawBrush, xPos, yPos);
+            if (!Directory.Exists(videoPath))
+                Directory.CreateDirectory(videoPath);
+
+            //创建文件路径
+            //fileFullPath = path + fileName;
+
+            if (stopREC)
+            {
+                stopREC = true;
+                createNewFile = true;  //这里要设置为true表示要创建新文件
+                if (videoWriter != null)
+                    videoWriter.Close();
+            }
+            else
+            {
+                //开始录像
+                if (createNewFile)
+                {
+                    videoFileName = DateTime.Now.ToString("yyyyMMddHH") + ".avi";
+                    videoFileFullPath = videoPath + videoFileName;
+                    createNewFile = false;
+                    if (videoWriter != null)
+                    {
+                        videoWriter.Close();
+                        videoWriter.Dispose();
+                    }
+                    videoWriter = new VideoFileWriter();
+                    //这里必须是全路径，否则会默认保存到程序运行根据录下了
+                    videoWriter.Open(videoFileFullPath, image.Width, image.Height, frameRate, VideoCodec.MPEG4);
+                    videoWriter.WriteVideoFrame(image);
+                }
+                else
+                {
+                    videoWriter.WriteVideoFrame(image);
+                }
+            }
+        }
+
+        private string GetImagePath()
+        {
+            string personImgPath = "";
+
+            personImgPath = this.tb_filePath.Text.Trim();
+            if (string.IsNullOrEmpty(personImgPath))
+            {
+                personImgPath = Path.GetDirectoryName(AppDomain.CurrentDomain.BaseDirectory)
+                              + Path.DirectorySeparatorChar.ToString() + "PersonImg";
+            }
+            if (!Directory.Exists(personImgPath))
+            {
+                Directory.CreateDirectory(personImgPath);
+            }
+
+            return personImgPath;
+        }
+
+
+        private void WriteLogsResult(object text)
+        {
+            this.toolStripStatusLabel1.Text = text.ToString();
+        }
+        private void SetLosResult(string text)
+        {
+            try
+            {
+                this.statusStrip1.Invoke(new WriteLabelDelegate(WriteLogsResult), text);
+            }
+            catch
+            {
+            }
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog dialog = new FolderBrowserDialog();
+            dialog.Description = "请选择文件路径";
+            if (dialog.ShowDialog() == DialogResult.OK)
+            {
+                string foldPath = dialog.SelectedPath;
+                this.tb_filePath.Text = foldPath;
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            //开始录像
+            if (btnStartVideotape.Text == "开始录像")
+            {
+                stopREC = false;
+                //frameRate = Convert.ToInt32(txtFrameRate.Text.Trim());
+                btnStartVideotape.Text = "停止录像";
+            }
+            else if (btnStartVideotape.Text == "停止录像")
+            {
+                stopREC = true;
+                btnStartVideotape.Text = "开始录像";
+            }
+        }
+        
+    }
+}
